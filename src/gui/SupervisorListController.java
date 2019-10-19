6fc55652ba18592b51ed5fa428a510f78d0116d7
Tomas Herculano <tomasherculano@gmail.com>
2019-10-19 14:25:15 -0300

    SupervisorForm (dialog) design

--- src/gui/SupervisorListController.java
+++ src/gui/SupervisorListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import app.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Supervisor;
 import model.entities.enums.PostoGrad;
@@ -49,8 +58,9 @@ public class SupervisorListController implements Initializable{
 	private ObservableList<Supervisor> obsList;
 	
 	@FXML
-	public void onBtNovoAction() {
-		System.out.println("onBtNovoAction");
+	public void onBtNovoAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/SupervisorForm.fxml", parentStage);
 	}
 
 	public void setSupervisorService (SupervisorService service) {
@@ -84,5 +94,23 @@ public class SupervisorListController implements Initializable{
 		List<Supervisor> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewSupervisor.setItems(obsList);
+	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Dados do Supervisor");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage); //init pai da janela
+			dialogStage.initModality(Modality.WINDOW_MODAL); //impede o acesso da janela anterior enquanto a mesma nao for fechada
+			dialogStage.showAndWait();			
+			
+		}catch(IOException e){
+			Alerts.showAlert("IO Exception", "Erro ao carregar a página", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
